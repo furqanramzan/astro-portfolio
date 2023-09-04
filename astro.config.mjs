628f31fb75f5fa67864b5a445ade99833ade52a8
Muhammad Furqan <furqanramzan271996@gmail.com>
2023-09-04 18:23:16 +0500

    feat(pwa): add pwa

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,43 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
+import AstroPWA from '@vite-pwa/astro';
+import { color, name, shortName } from './src/utils/constants';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    AstroPWA({
+      base: '/',
+      scope: '/',
+      includeAssets: ['favicon.svg'],
+      registerType: 'autoUpdate',
+      manifest: {
+        name,
+        short_name: shortName,
+        theme_color: color,
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ['**/*.{css,js,html,svg,png,webp,ico}'],
+      },
+    }),
+  ],
 });
---
+++ src/utils/constants.ts
@@ -0,0 +1,5 @@
+export const name = 'Fareed Hassan';
+
+export const shortName = 'Fareed';
+
+export const color = '#111827';
