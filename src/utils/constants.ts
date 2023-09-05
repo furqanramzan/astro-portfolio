d282f54f81f1bc75081693931c52caa13dd009ae
Muhammad Furqan <furqanramzan271996@gmail.com>
2023-09-05 17:06:46 +0500

    feat(navigation): add navigation buttons component

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,3 +3,14 @@ export const name = 'Fareed Hassan';
 export const shortName = 'Fareed';
 
 export const color = '#111827';
+
+export const links = {
+  home: { name: 'Home', href: '/' },
+  about: { name: 'About', href: '/about' },
+  tool: { name: 'Tools and Platforms', href: '/tool' },
+  experience: { name: 'Experience', href: '/experience' },
+};
+type Links = typeof links;
+type LinkKey = keyof Links;
+
+export const navLinks: LinkKey[] = ['about', 'tool', 'experience'];
